ef8125eec8e94afe0bdac696b0d3305b4da1f643
Marabii <minehamza97@gmail.com>
2024-05-17 22:14:26 +0200

    reconfiguring sessions to work in production mode

--- app.cjs
+++ app.cjs
@@ -87,6 +87,9 @@ app.use(
     saveUninitialized: false,
     store: sessionStore,
     cookie: {
+      domain: process.env.FRONT_END,
+      secure: true,
+      httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec)
     },
   })
