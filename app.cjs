3a413224d6ace7086f0a7744c23ec9e54bd98da5
Marabii <109368243+Marabii@users.noreply.github.com>
2024-06-08 11:49:10 +0200

    Update app.cjs

--- app.cjs
+++ app.cjs
@@ -59,6 +59,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
+console.log("FRONT_END", process.env.FRONT_END);
+
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
